ffb990acbde9258edfeb263534113e3e046f7a6c
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-10 20:45:36 -0500

    addeing unknown

--- src/modules/saved-search.test.ts
+++ src/modules/saved-search.test.ts
@@ -1,23 +1,38 @@
-import ModuleSaveAction, {SaveSearch} from "./saved-search"
+import ModuleSaveAction, { SaveSearch } from './saved-search';
 
-const buildSaveSearch= ():SaveSearch =>({
-   listingTypes: 'sell',
-   TownId: [],
-   propertyTypes:['Land']
-})
+const buildSaveSearch = (): SaveSearch => ({
+    listingTypes: 'sell',
+    TownId: [],
+    propertyTypes: ['Land'],
+});
 
+describe('SaveSearch', () => {
+    test(' detect changes on string', () => {
+        const before: SaveSearch = {
+            ...buildSaveSearch(),
+            listingTypes: 'sell',
+        };
+        const after: SaveSearch = {
+            ...buildSaveSearch(),
+            listingTypes: 'rent',
+        };
 
-describe("SaveSearch", ()=>{
-   test(" detect changes on string", ()=>{
- const before:SaveSearch = {...buildSaveSearch(), listingTypes: "sell"}
- const after:SaveSearch = {...buildSaveSearch(), listingTypes: "rent"}
+        const result = {
+            listingTypes: {
+                before: 'sell',
+                after: 'rent',
+            },
+        };
+        expect(ModuleSaveAction.difference(before, after)).toEqual(result);
+    });
+});
 
- const result = {
-   listingTypes :{
-      before:"sell",
-      after: "rent",
-   },
- }
- expect(ModuleSaveAction.difference(before, after)).toEqual(result)
-   })
-})
+test(' if string remain the same', () => {
+    const before: SaveSearch = buildSaveSearch();
+    const after: SaveSearch = buildSaveSearch();
+
+    const result = {
+        listingTypes: undefined,
+    };
+    expect(ModuleSaveAction.difference(before, after)).toEqual(result);
+});
--- src/modules/saved-search.ts
+++ src/modules/saved-search.ts
@@ -1,34 +1,31 @@
 export type SaveSearch = {
+    listingTypes: 'rent' | 'sell';
+    TownId: string[];
+    propertyTypes: (
+        | 'House'
+        | 'Condo'
+        | 'Townhouse'
+        | 'Multi-Family'
+        | 'Land'
+        | 'Mobile/Manufactured'
+    )[];
+};
 
-   listingTypes : "rent" | "sell",
-   TownId: string [],
-   propertyTypes: ("House"| "Condo"| "Townhouse" | "Multi-Family" | "Land" | "Mobile/Manufactured" )[]
-}
-
-
-const diffStr = (before:string, after:string)=>{
-
-   if(before === after){
-      return undefined
-   }else{
-      return {before, after}
-   }
-
-
-}
+const diffStr = (before: string, after: string) => {
+    if (before === after) return undefined;
+    return { before, after };
+};
 
 //create a diff, to checked for changes on users account
-const difference = (before:SaveSearch, after:SaveSearch)=>{
-
-
-}
+const difference = (before: SaveSearch, after: SaveSearch) => {
+    const result = {
+        listingTypes: diffStr(before.listingTypes, after.listingTypes),
+    };
+    return result;
+};
 
 const ModuleSaveAction = {
-
-difference
-
-}
-
-
+    difference,
+};
 
 export default ModuleSaveAction;
